d8afa935541eeb9d9abb83220bfcbe60e5545c53
realdev04 <nimsarachamidhu@gmail.com>
2021-10-20 11:45:02 +0530

    Removed Unused import and added title in index.html

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,10 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import Home from './pages/Home';
 import NavBar from './components/Navbar';
 import PRImg from '../src/assets/icons/me-logo.png';
 
 function App() {
 
-  useEffect(()=>{
-    document.title = "Match Eka";
-  },[]);
-  
   return (
     <div className="App">
       <NavBar imgSrc={PRImg}/>
