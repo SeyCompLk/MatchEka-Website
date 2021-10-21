7cbdfda9c5108f75cc4c3859633d22adef46cbb5
Shakya Peiris <shakyaimanjith32@gmail.com>
2021-10-21 18:23:50 +0530

    wrap with auth context and routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import Home from './pages/Home';
-import NavBar from './components/Navbar';
+import NavBar from './components/UI/Navbar';
 import PRImg from '../src/assets/icons/me-logo.png';
+import { authContext } from './context/authContext';
+import { Route } from 'react-router-dom';
 
 function App() {
-
+  const ctx = useContext(authContext);
   return (
     <div className="App">
-      <NavBar imgSrc={PRImg}/>
-      <Home/>
+      <NavBar imgSrc={PRImg}>
+        <Route path="/">
+          <Home />
+        </Route>
+      </NavBar>
     </div>
   );
 }
