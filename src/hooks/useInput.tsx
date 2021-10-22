1919c5bc4f44514d035dabe5018a8895585388ff
Shakya Peiris <shakyaimanjith32@gmail.com>
2021-10-22 22:30:03 +0530

    add input focus ability

--- src/hooks/useInput.tsx
+++ src/hooks/useInput.tsx
@@ -1,32 +1,40 @@
-import { ChangeEvent, useState } from "react"
+import { ChangeEvent, useRef, useState } from 'react';
 
-const useInput = (validator:(inputVale: string) => boolean) => {
-    const [inputValue, setInputValue] = useState('')
-    const [inputTouched, setInputTouched] = useState(false)
-    const isInputValid = validator(inputValue)
-    const hasError = !isInputValid && inputTouched
+const useInput = (validator: (inputVale: string) => boolean) => {
+  const [inputValue, setInputValue] = useState('');
+  const [inputTouched, setInputTouched] = useState(false);
+  const inputRef = useRef<HTMLInputElement>();
+  const isInputValid = validator(inputValue);
+  const hasError = !isInputValid && inputTouched;
 
-    const valueChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
-        setInputValue(event.target.value)
-    }
+  const valueChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
+    setInputValue(event.target.value);
+  };
 
-    const inputBlurHandler = () => {
-        setInputTouched(true)
-    }
+  const inputBlurHandler = () => {
+    setInputTouched(true);
+  };
 
-    const reset = () => {
-        setInputValue('')
-        setInputTouched(false)
-    }
+  const focusHandler = () => {
+    setInputTouched(true);
+    inputRef.current?.focus();
+  };
 
-    return {
-        inputValue,
-        isInputValid,
-        hasError,
-        valueChangeHandler,
-        inputBlurHandler,
-        reset
-    }
-}
+  const reset = () => {
+    setInputValue('');
+    setInputTouched(false);
+  };
 
-export default useInput
+  return {
+    inputValue,
+    isInputValid,
+    hasError,
+    valueChangeHandler,
+    inputBlurHandler,
+    reset,
+    inputRef,
+    focusHandler,
+  };
+};
+
+export default useInput;
