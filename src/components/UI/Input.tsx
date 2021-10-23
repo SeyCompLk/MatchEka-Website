ec6807f6cd8f186cae56dcce397ab078783524da
Shakya Peiris <shakyaimanjith32@gmail.com>
2021-10-23 13:00:05 +0530

    implement basic stylings of Input component

--- src/components/UI/Input.tsx
+++ src/components/UI/Input.tsx
@@ -4,7 +4,7 @@ import { InputProps } from '../../types';
 import classes from './Input.module.css';
 
 const Input = forwardRef((props: InputProps, ref) => {
-  const inputRef = useRef<HTMLInputElement>();
+  const inputRef = useRef<HTMLInputElement>(null);
 
   const activate = () => {
     inputRef.current?.focus();
@@ -17,18 +17,19 @@ const Input = forwardRef((props: InputProps, ref) => {
   });
 
   return (
-    <>
+    <div className={classes.Container}>
       {props.label && <label>{props.label}</label>}
       <input
         type={props.type}
         value={props.value}
         onChange={props.onChange}
         onBlur={props.onBlur}
-        //ref={inputRef}
+        ref={inputRef}
         className={classes.Input}
+        placeholder={props.placeholder}
         style={props.style}
       />
-    </>
+    </div>
   );
 });
 
