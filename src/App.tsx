5f0cbaea31204a400e0d768ff0ec65ee3a194513
Shakya Peiris <shakyaimanjith32@gmail.com>
2021-10-25 12:13:56 +0530

    implement seperate routings for user auth

--- src/App.tsx
+++ src/App.tsx
@@ -6,27 +6,35 @@ import UserLogin from './pages/Login';
 import UserRegister from './pages/SignUp';
 import Leaderboard from './pages/Leaderboard';
 import { authContext } from './context/authContext';
-import { Route, Redirect } from 'react-router-dom';
+import { Route, Redirect, useLocation } from 'react-router-dom';
 import './App.css';
 
 function App() {
   const ctx = useContext(authContext);
+  const router = useLocation();
   console.log(ctx.token);
   return (
-    <NavBar imgSrc={PRImg}>
-      <Route path="/" exact>
-        <Home />
-      </Route>
-      <Route path="/leaderboard" exact>
-        <Leaderboard />
-      </Route>
+    <>
       <Route path="/user/login">
         {ctx.token === null ? <UserLogin /> : <Redirect to="/" />}
       </Route>
       <Route path="/user/register">
         {ctx.token === null ? <UserRegister /> : <Redirect to="/" />}
       </Route>
-    </NavBar>
+      {!(
+        router.pathname === '/user/login' ||
+        router.pathname === '/user/register'
+      ) && (
+        <NavBar imgSrc={PRImg}>
+          <Route path="/" exact>
+            <Home />
+          </Route>
+          <Route path="/leaderboard" exact>
+            <Leaderboard />
+          </Route>
+        </NavBar>
+      )}
+    </>
   );
 }
 
