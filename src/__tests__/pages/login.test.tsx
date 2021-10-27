3b06007a7fa47eb26ae12b7f97d0f6578ad387db
Shakya Peiris <shakyaimanjith32@gmail.com>
2021-10-27 16:38:18 +0530

    write tests for login page

--- src/__tests__/pages/login.test.tsx
+++ src/__tests__/pages/login.test.tsx
@@ -1,6 +1,14 @@
 import React from 'react';
 import { render, fireEvent, cleanup } from '@testing-library/react';
+import Login from '../../pages/Login';
 
 afterAll(() => {
   cleanup();
+});
+
+describe('Intergration tests of /login', () => {
+  test('Should match snapshot', () => {
+    const LoginPage = render(<Login />);
+    expect(LoginPage).toMatchSnapshot();
+  });
 });
