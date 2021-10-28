fda6f6d28f667043779b6af132f7bf3b9e5a73cc
Shakya Peiris <shakyaimanjith32@gmail.com>
2021-10-28 16:32:17 +0530

    setup types for match data

--- src/types.ts
+++ src/types.ts
@@ -70,3 +70,121 @@ export interface LeaderboardProps {
   prevMonth: string;
   prevWeek: string;
 }
+
+export interface MatchOverviewProps {
+  team1: string | undefined;
+  team2: string | undefined;
+  inning: number | undefined;
+  score: number | undefined;
+  wickets: number | undefined;
+  overs: number | undefined;
+  bowls: number | undefined;
+  currStriker:
+    | {
+        name: string;
+        score: number;
+        bowls: number;
+      }
+    | undefined;
+  nonStriker:
+    | {
+        name: string;
+        score: number;
+        bowls: number;
+      }
+    | undefined;
+  currBowler:
+    | {
+        name: string;
+        overs: number;
+        wickets: number;
+      }
+    | undefined;
+}
+
+interface Player {
+  _id: string;
+  name: string;
+  imageUrl: string;
+  role: string;
+  battingArm: string;
+  bowlingArm: string;
+}
+
+interface PlayingPlayer extends Player {
+  playerId: string;
+}
+
+interface InningData {
+  battingTeam: number;
+  bowlingTeam: number;
+
+  bat: {
+    name: string;
+    status: string;
+    score: number;
+    bowlsFaced: number;
+  }[];
+  bowl: {
+    name: string;
+    overs: number;
+    wickets: number;
+  }[];
+  overs: number;
+  wickets: number;
+  totalScore: number;
+  bowls: number;
+}
+
+export interface ScoreBoardResponse {
+  title: string;
+  venue: string;
+  date: string;
+  teams: {
+    team1: {
+      country: string;
+      flag: string;
+      squad: Player[];
+    };
+    team2: {
+      country: string;
+      flag: string;
+      squad: Player[];
+    };
+  };
+  scoreBoard: {
+    inning: number;
+    tossWon: number;
+    toss: {
+      team1: string;
+      team2: string;
+    };
+    teams: {
+      team1: PlayingPlayer[];
+      team2: PlayingPlayer[];
+    };
+    scores: {
+      inn1: InningData;
+      inn2: InningData;
+    };
+    batsman: {
+      striker: {
+        name: string;
+        score: number;
+        bowls: number;
+      };
+      nonStriker: {
+        name: string;
+        score: number;
+        bowls: number;
+      };
+    };
+    currBowler: {
+      name: string;
+      overs: number;
+      wickets: number;
+    };
+  };
+  isLive: boolean;
+  overs: number;
+}
