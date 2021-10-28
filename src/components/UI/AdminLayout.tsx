d8250a92c3f6fddae216f61fca5c8d9ccdddbdbb
Shakya Peiris <shakyaimanjith32@gmail.com>
2021-10-28 16:33:03 +0530

    create admin layout

---
+++ src/components/UI/AdminLayout.tsx
@@ -0,0 +1,43 @@
+import React, { ReactElement, useContext } from 'react';
+import classes from './AdminLayout.module.css';
+import Logo from '../../assets/icons/me-logo.png';
+import { NavLink } from 'react-router-dom';
+import { authContext } from '../../context/authContext';
+
+const AdminLayout = (props: { children: ReactElement | ReactElement[] }) => {
+  const ctx = useContext(authContext);
+  return (
+    <header className={classes.Header}>
+      <div className={classes.Brand}>
+        <img src={Logo} alt="logo" />
+        <span className={classes.BrandName}>Match Eka</span>
+        <span>Admin</span>
+      </div>
+      <nav className={classes.Nav}>
+        <ul>
+          <li>
+            <NavLink to="/admin/" activeClassName={classes.Active}>
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/admin/new-match" activeClassName={classes.Active}>
+              New match
+            </NavLink>
+          </li>
+          {ctx.isAdmin && ctx.token && (
+            <li
+              onClick={() => {
+                ctx.logout();
+              }}
+            >
+              Logout
+            </li>
+          )}
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default AdminLayout;
