add0b5861c14c0ef4cbf3919a9c1a481d73bb5e3
Shakya Peiris <shakyaimanjith32@gmail.com>
2021-10-28 16:32:45 +0530

    setup layout and routes

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,10 @@ import PRImg from '../src/assets/icons/me-logo.png';
 import UserLogin from './pages/Login';
 import UserRegister from './pages/SignUp';
 import Leaderboard from './pages/Leaderboard';
+import Chat from './pages/Chat';
+import AdminLayout from './components/UI/AdminLayout';
 import AdminHome from './pages/Admin/Home';
+import AdminSingleMatch from './pages/Admin/SingleMatch';
 import { authContext } from './context/authContext';
 import { Route, Redirect, useLocation } from 'react-router-dom';
 import './App.css';
@@ -23,9 +26,14 @@ function App() {
         {ctx.token === null ? <UserRegister /> : <Redirect to="/" />}
       </Route>
       <Route path="/admin">
-        <Route path="/">
-          <AdminHome />
-        </Route>
+        <AdminLayout>
+          <Route path="/" exact>
+            <AdminHome />
+          </Route>
+          <Route path="/matches/:matchId">
+            <AdminSingleMatch />
+          </Route>
+        </AdminLayout>
       </Route>
       {!(
         router.pathname === '/user/login' ||
@@ -38,6 +46,9 @@ function App() {
           </Route>
           <Route path="/leaderboard" exact>
             <Leaderboard />
+          </Route>
+          <Route path="/chat/:matchId">
+            <Chat />
           </Route>
         </NavBar>
       )}
