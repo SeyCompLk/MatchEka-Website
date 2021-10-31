4822a50526347081ea75abb01aee41a494ebd768
Shakya Peiris <shakyaimanjith32@gmail.com>
2021-10-31 18:32:50 +0530

    fix issues with routing

--- src/pages/Admin/SingleMatch.tsx
+++ src/pages/Admin/SingleMatch.tsx
@@ -1,5 +1,7 @@
-import React, { useCallback, useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import React, { useCallback, useContext, useEffect, useState } from 'react';
+import { useParams, Switch, Route } from 'react-router-dom';
+import Overview from '../../components/Admin/Overview';
+import { authContext } from '../../context/authContext';
 import { MatchOverviewProps, ScoreBoardResponse } from '../../types';
 
 const SingleMatch = () => {
@@ -43,15 +45,23 @@ const SingleMatch = () => {
 
   //data fetcing
   const [loading, setLoading] = useState(false);
+  const ctx = useContext(authContext);
 
   const fetchData = useCallback(async () => {
     setLoading(true);
-    const url = `${process.env.REACT_APP_API_ENDPOINT}admin/matches/${matchId}`;
+    const url = `${process.env.REACT_APP_API_ENDPOINT}admin/matches/${matchId}&token=${ctx.token}`;
     const response = await fetch(url);
     const data: ScoreBoardResponse = await response.json();
-    setFullData(data);
+    if (!data.isLive) {
+      setLoading(false);
+    } else {
+      setFullData(data);
+    }
     setLoading(false);
-  }, [matchId]);
+  }, [matchId, ctx]);
+
+  // 6171734c303f363e5a14c05d
+  // eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InNoYWt5YWltYW5qaXRoMzJAZ21haWwuY29tIiwiaXNBZG1pbiI6dHJ1ZSwiaWF0IjoxNjM0ODIzNTc4LCJleHAiOjE2MzQ5OTYzNzh9.0OyJNjs7NDfYlK8DugnTMQAzlIOhIQuuzZNLnWoMH0s
 
   useEffect(() => {
     fetchData();
@@ -62,13 +72,27 @@ const SingleMatch = () => {
       {loading ? (
         <h1>Loading...</h1>
       ) : (
-        overview && (
-          <div>
-            <h1>
-              {overview.team1} vs {overview.team2}
-            </h1>
-          </div>
-        )
+        <>
+          <div></div>
+          <Switch>
+            <Route path="/" exact>
+              <Overview
+                team1="Sri Lanka"
+                team2="South Africa"
+                inning={1}
+                score={50}
+                wickets={5}
+                overs={15}
+                bowls={3}
+                currStriker={{ name: 'Shakya', score: 25, bowls: 32 }}
+                nonStriker={{ name: 'Shakya', score: 25, bowls: 32 }}
+                currBowler={{ name: 'Sakithma', overs: 10, wickets: 3 }}
+              />
+            </Route>
+            <Route path="/full-score"></Route>
+            <Route path="/toss"></Route>
+          </Switch>
+        </>
       )}
     </>
   );
