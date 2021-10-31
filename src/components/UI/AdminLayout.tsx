50de25037af8ea01ae1f1fdd99473bfdcf6cdbd5
Shakya Peiris <shakyaimanjith32@gmail.com>
2021-10-31 18:33:55 +0530

    update admin layout

--- src/components/UI/AdminLayout.tsx
+++ src/components/UI/AdminLayout.tsx
@@ -7,36 +7,41 @@ import { authContext } from '../../context/authContext';
 const AdminLayout = (props: { children: ReactElement | ReactElement[] }) => {
   const ctx = useContext(authContext);
   return (
-    <header className={classes.Header}>
-      <div className={classes.Brand}>
-        <img src={Logo} alt="logo" />
-        <span className={classes.BrandName}>Match Eka</span>
-        <span>Admin</span>
-      </div>
-      <nav className={classes.Nav}>
-        <ul>
-          <li>
-            <NavLink to="/admin/" activeClassName={classes.Active}>
-              Home
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to="/admin/new-match" activeClassName={classes.Active}>
-              New match
-            </NavLink>
-          </li>
-          {ctx.isAdmin && ctx.token && (
-            <li
-              onClick={() => {
-                ctx.logout();
-              }}
-            >
-              Logout
+    <>
+      <header className={classes.Header}>
+        <div className={classes.Brand}>
+          <img src={Logo} alt="logo" />
+          <span className={classes.BrandName}>Match Eka</span>
+          <span>|Admin</span>
+        </div>
+        <nav className={classes.Nav}>
+          <ul>
+            <li>
+              <NavLink to="/admin/" activeClassName={classes.Active}>
+                Home
+              </NavLink>
             </li>
-          )}
-        </ul>
-      </nav>
-    </header>
+            <li>
+              {ctx.isAdmin && ctx.token && (
+                <NavLink to="/admin/new-match" activeClassName={classes.Active}>
+                  New match
+                </NavLink>
+              )}
+            </li>
+            {ctx.isAdmin && ctx.token && (
+              <li
+                onClick={() => {
+                  ctx.logout();
+                }}
+              >
+                Logout
+              </li>
+            )}
+          </ul>
+        </nav>
+      </header>
+      <div>{props.children}</div>
+    </>
   );
 };
 
