79d560d210fa52010b944814e4bfbd4ccaeaca4a
Shakya Peiris <shakyaimanjith32@gmail.com>
2021-10-31 19:49:44 +0530

    Revert "setup layout and routes"
    This reverts commit 16da26bdb1346c67bf405d978bebd01250bcd1b7.

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,7 @@ import PRImg from '../src/assets/icons/me-logo.png';
 import UserLogin from './pages/Login';
 import UserRegister from './pages/SignUp';
 import Leaderboard from './pages/Leaderboard';
-import Chat from './pages/Chat';
-import AdminLayout from './components/UI/AdminLayout';
 import AdminHome from './pages/Admin/Home';
-import AdminSingleMatch from './pages/Admin/SingleMatch';
 import { authContext } from './context/authContext';
 import { Route, Redirect, useLocation } from 'react-router-dom';
 import './App.css';
@@ -26,14 +23,9 @@ function App() {
         {ctx.token === null ? <UserRegister /> : <Redirect to="/" />}
       </Route>
       <Route path="/admin">
-        <AdminLayout>
-          <Route path="/" exact>
-            <AdminHome />
-          </Route>
-          <Route path="/matches/:matchId">
-            <AdminSingleMatch />
-          </Route>
-        </AdminLayout>
+        <Route path="/">
+          <AdminHome />
+        </Route>
       </Route>
       {!(
         router.pathname === '/user/login' ||
@@ -46,9 +38,6 @@ function App() {
           </Route>
           <Route path="/leaderboard" exact>
             <Leaderboard />
-          </Route>
-          <Route path="/chat/:matchId">
-            <Chat />
           </Route>
         </NavBar>
       )}
