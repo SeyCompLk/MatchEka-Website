9a60314ebdb2d13b8e5cfe3ab4143228aa3d6a27
Shakya Peiris <shakyaimanjith32@gmail.com>
2021-11-30 15:43:47 +0530

    create the basic structure of chats page

--- src/components/Chat/Messages.tsx
+++ src/components/Chat/Messages.tsx
@@ -1,7 +1,27 @@
 import React from 'react';
+import classes from './Style.module.css';
 
 const Messages = () => {
-  return <div></div>;
+  const messages = [
+    'hi',
+    'sup',
+    "how's the match?",
+    'pretty good man!',
+    'will we win this game?',
+  ];
+  return (
+    <div className={classes.ChatBox}>
+      <div>
+        {messages.map((message) => (
+          <p>{message}</p>
+        ))}
+      </div>
+      <form>
+        <input placeholder="Enter a message" />
+        <button>Send</button>
+      </form>
+    </div>
+  );
 };
 
 export default Messages;
--- src/pages/Chat.tsx
+++ src/pages/Chat.tsx
@@ -1,13 +1,19 @@
 import React from 'react';
+import { useLocation } from 'react-router-dom';
 import MatchData from '../components/Chat/MatchData';
 import Messages from '../components/Chat/Messages';
 import Overlay from '../components/UI/Overlay';
+import classes from '../styles/Chat.module.css';
 // import { useParams } from 'react-router-dom';
 
 const Chat = () => {
+  const { search } = useLocation();
+  const query = new URLSearchParams(search);
+  const match = query.get('match');
+  console.log(match);
   return (
     <Overlay>
-      <div>
+      <div className={classes.Container}>
         <MatchData />
         <Messages />
       </div>
